ecd26a2b7d364ba4f3c83a65c2198493a250aa63
Olivier Levillain <olivier.levillain@picty.org>
2011-10-31 20:53:04 +0100

    Added some extensions...

--- asn1/x509Extensions.ml
+++ asn1/x509Extensions.ml
@@ -4,6 +4,8 @@ open Asn1
 open Asn1Constraints
 
 
+(* Basic Constraints *)
+
 let basicConstraints_oid = [85;29;19]
 
 let bc_constraint = {
@@ -26,9 +28,44 @@ let mkBasicConstraints = Simple_cons (C_Universal, true, 16, "basicConstraints",
 
 
 
+
+(* Subject Key Identifier *)
+
 let subjectKeyIdentifier_oid = [85;29;14]
+
+let mkSKI = Simple_cons (C_Universal, false, 4, "subjectKeyIdentifier",
+			 fun pstate -> SubjectKeyIdentifier (Asn1.Engine.pop_string pstate))
+
+
+
+(* Authority Key Identifier *)
 let authorityKeyIdentifier_oid = [85;29;35]
+
+let aki_constraint = {
+  severity_if_too_many_objects = s_specfatallyviolated;
+  constraint_list = [
+    Simple_cons (C_ContextSpecific, false, 0, "keyIdentifier", der_to_octetstring true), s_ok;
+    Simple_cons (C_ContextSpecific, true, 1, "authorityCertIssuer", der_to_constructed), s_ok;
+    Simple_cons (C_ContextSpecific, false, 2, "authorityCertSerialNumber", der_to_int), s_ok
+  ]
+}
+
+let extract_aki = function
+  | (String (s, true))::_ -> AuthorityKeyIdentifier (AKI_KeyIdentifier s)
+  (* TODO *)
+  | _ -> AuthorityKeyIdentifier AKI_Unknown
+
+let mkAKI = Simple_cons (C_Universal, true, 16, "basicConstraints",
+			 parse_constrained_sequence extract_aki aki_constraint)
+
+
+
+
+(* CRL Distribution Point *)
+
 let crlDistributionPoint_oid = [85;29;31]
+
+
 let authorityInfoAccess_oid = [43;6;1;5;5;7;1;1]
 let keyUsage_oid = [85;29;15]
 let extKeyUsage_oid = [85;29;37]
@@ -36,18 +73,70 @@ let ocsp_oid = [43;6;1;5;5;7;48;1]
 
 
 
-(*let mkSKI s =
-  let asn1struct = Asn1.exact_parse s in
-  match asn1struct with
-    | (Asn1.C_Universal, 4, Asn1.String ki) -> SubjectKeyIdentifier ki
-    | _ -> failwith "Invalid subject key identifier"
 
-let mkAKI s =
+
+
+
+
+
+(*let dp_constraint = {
+  severity_if_too_many_objects = s_specfatallyviolated;
+  constraint_list = [
+    Simple_cons (C_ContextSpecific, true, 0, "DistributionPointName", der_to_constructed), s_ok;
+    Simple_cons (C_ContextSpecific, false, 1, "ReasonFlags", der_to_bitstring), s_ok;
+    Simple_cons (C_ContextSpecific, true, 2, "cRLIssuer", der_to_constructed), s_ok
+  ]
+}
+
+let extract_dp = 
+
+let distributionPoint_cons = Simple_cons (C_Universal, true, 16, "distributionPoint",
+					  parse_constrained_sequence extract_dp dp_constraint)
+
+let mkCRLDP = Simple_cons (C_Universal, true, 16, "cRLDistributionPoint",
+			   parse_sequenceof Common.identity distributionPoint_cons (AtLeast (1, s_speclightlyviolated)))
+
+parse_sequenceof (postprocess : 'a list -> 'b) (cons : 'a asn1_constraint)
+                         (n : number_constraint) (pstate : parsing_state) : 'b =
+  
+/* From RFC 5280
+DistributionPointName ::= CHOICE {
+  fullName                [0]     GeneralNames,
+  nameRelativeToCRLIssuer [1]     RelativeDistinguishedName
+}
+
+What is really coded here is:
+DistributionPointName ::= SEQUENCE {
+  fullName                [0]     GeneralNames OPTIONAL,
+  nameRelativeToCRLIssuer [1]     RelativeDistinguishedName OPTIONAL
+}
+*/
+
+/* From RFC 5280
+DistributionPoint ::= SEQUENCE {
+  distributionPoint       [0]     DistributionPointName OPTIONAL,
+  reasons                 [1]     ReasonFlags OPTIONAL,
+  cRLIssuer               [2]     GeneralNames OPTIONAL
+}
+*/
+CRLDistributionPoints ::= SEQUENCE SIZE (1..MAX) OF DistributionPoint
+
+
+
+let mkCRLDistributionPoint s =
   let asn1struct = Asn1.exact_parse s in
   match asn1struct with
     | (Asn1.C_Universal, 16, Asn1.Constructed
-      [(Asn1.C_ContextSpecific, 0, Asn1.Unknown ki)]) -> AuthorityKeyIdentifier (AKI_KeyIdentifier ki)
-    | _ -> AuthorityKeyIdentifier (AKI_Unknown)
+      [(Asn1.C_Universal, 16, Asn1.Constructed
+	[(Asn1.C_ContextSpecific, 0, Asn1.Constructed
+	  [(Asn1.C_ContextSpecific, 0, Asn1.Constructed
+	    [(Asn1.C_ContextSpecific, 6, Asn1.Unknown url)])])])]) -> CRLDistributionPoint url
+    | _ -> failwith "Invalid or unknown CRL distribution point"*)
+
+
+
+
+(*
 
 let mkCRLDistributionPoint s =
   let asn1struct = Asn1.exact_parse s in
@@ -93,9 +182,9 @@ let add_extensions () =
   Hashtbl.add name_directory basicConstraints_oid "basicConstraints";
   Hashtbl.add extension_directory basicConstraints_oid mkBasicConstraints;
   Hashtbl.add name_directory subjectKeyIdentifier_oid "subjectKeyIdentifier";
-(*  Hashtbl.add extension_directory subjectKeyIdentifier_oid mkSKI;*)
+  Hashtbl.add extension_directory subjectKeyIdentifier_oid mkSKI;
   Hashtbl.add name_directory authorityKeyIdentifier_oid "authorityKeyIdentifier";
-(*  Hashtbl.add extension_directory authorityKeyIdentifier_oid mkAKI;*)
+  Hashtbl.add extension_directory authorityKeyIdentifier_oid mkAKI;
   Hashtbl.add name_directory crlDistributionPoint_oid "crlDistributionPoint";
 (*  Hashtbl.add extension_directory crlDistributionPoint_oid mkCRLDistributionPoint;*)
   Hashtbl.add name_directory authorityInfoAccess_oid "authorityInfoAccess";
