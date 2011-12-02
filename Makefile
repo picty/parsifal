2bfe649d4082aa17c94010462579a4f561f301e2
Olivier Levillain <olivier.levillain@picty.org>
2011-12-02 13:40:32 +0100

    First implementation of a transparent proxy. The program only parse records at a low level for the moment. We need sordid hacks to have the program compile properly.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	ocamlbuild facesl.native asn1parse.native test_asn1.native test_asn1Parser.native test_crypto.native test_answerDump.native test_socket.native test_record.native testBgp.native test_x509.native
+	ocamlbuild -cflags -I,+lwt -lflags -I,+lwt -libs unix,bigarray,lwt,lwt-unix facesl.native asn1parse.native test_asn1.native test_asn1Parser.native test_crypto.native test_answerDump.native test_socket.native test_record.native testBgp.native test_x509.native sslproxy.native
 
 clean:
 	ocamlbuild -clean
--- sslproxy.ml
+++ sslproxy.ml
@@ -6,6 +6,21 @@ open Unix
 (* TODO: Handle Unix exceptions *)
 
 
+
+type tls_state = {
+  name : string;
+
+  mutable clear : bool;
+  mutable cur_buf : string;
+  mutable cur_records : TlsRecord.RecordParser.t list;
+}
+
+let empty_state name =
+  { name = name; clear = true;
+    cur_buf = ""; cur_records = [] }
+
+
+
 (* write l bytes of string s
  * starting at position p to file descriptor o.
  * Several calls to function Lwt_unix.write may
@@ -20,14 +35,45 @@ let rec really_write o s p l =
     really_write o s (p + n) (l - n)
 
 
+let parse_records s =
+  let pstate = ParsingEngine.pstate_of_string None s.cur_buf in
 
-let rec forward i o =
-  let s = String.create 16 in
-  Lwt_unix.read i s 0 16 >>= fun l ->
+  let rec aux accu =
+    let saved_pstate = ParsingEngine.clone_pstate pstate in
+    try
+      let record = TlsRecord.RecordParser.parse pstate in
+      aux (record::accu)
+    with ParsingEngine.OutOfBounds _ ->
+      s.cur_buf <- ParsingEngine.pop_string saved_pstate;
+      s.cur_records <- List.rev accu
+  in
+  aux (List.rev s.cur_records)
+
+
+
+let write_record o record =
+  let s = TlsRecord.RecordParser.dump record in
+  let l = String.length s in
+  really_write o s 0 l
+
+let print_record record =
+  print_endline (String.concat "\n" (TlsRecord.RecordParser.to_string record))
+
+let handle_records state =
+  let records = state.cur_records in
+  state.cur_records <- [];
+  List.iter print_record records;
+  records
+
+let rec forward state i o =
+  let new_buf = String.make 1024 ' ' in
+  Lwt_unix.read i new_buf 0 1024 >>= fun l ->
   if l > 0 then begin
-    print_endline (Common.hexdump s);
-    really_write o s 0 l >>= fun () ->
-    forward i o
+    state.cur_buf <- state.cur_buf ^ (String.sub new_buf 0 l);
+    parse_records state;
+    let records = handle_records state in
+    (Lwt_list.iter_s (write_record o) records) >>= fun () ->
+    forward state i o
   end else begin
     Lwt_unix.shutdown o Unix.SHUTDOWN_SEND;
     Lwt.return ()
@@ -52,11 +98,11 @@ let rec accept sock =
   ignore
     (let out = new_socket () in
      Lwt_unix.connect out remote_addr >>= fun () ->
-     let io = forward inp out in
-     let oi = forward out inp in
+     let io = forward (empty_state "C->S") inp out in
+     let oi = forward (empty_state "S->C") out inp in
      io >>= fun () -> oi >>= fun () ->
-     Lwt_unix.close out;
-     Lwt_unix.close inp;
+     ignore (Lwt_unix.close out);
+     ignore (Lwt_unix.close inp);
      Lwt.return ());
   accept sock
 
