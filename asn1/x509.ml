a7e174468696f10327b533f4699ce6a28d07a512
Olivier Levillain <olivier.levillain@picty.org>
2011-12-09 18:34:55 +0100

    Added X509_object.is_ca function. It takes into account the version, and some extensions (basicConstraints, keyUsage, nsCertType). The verdict can be Yes, MostlyYes, MostlyNo, No, depending on the combinations encountered.

--- asn1/x509.ml
+++ asn1/x509.ml
@@ -203,18 +203,87 @@ let rec _get_extension exts oid = match exts with
     then ExtensionModule.register e
     else _get_extension r oid
 
-let get_extension exts ext_id =
-  let oid = match ext_id with
-    | V_List oid -> Asn1Parser.oid_of_list oid
-    | V_String s -> begin
-      try Hashtbl.find rev_name_directory s
-      with Not_found -> begin
-	try oid_squash (List.map int_of_string (string_split '.' s))
-	with Failure "int_of_string" -> raise (ContentError ("Unknown OId"))
-      end
-    end
-    | _ -> raise (ContentError ("OId must be strings or int list"))
-  in _get_extension exts oid
+let rec _get_content_extension exts oid = match exts with
+  | [] -> V_Unit
+  | e::r ->
+    if e.e_id = oid
+    then e.e_content
+    else _get_content_extension r oid
+
+let get_extension exts_container ext_id =
+  match exts_container with
+    | None -> V_Unit
+    | Some exts ->
+      let oid = match ext_id with
+	| V_List oid -> Asn1Parser.oid_of_list oid
+	| V_String s -> begin
+	  try Hashtbl.find rev_name_directory s
+	  with Not_found -> begin
+	    try oid_squash (List.map int_of_string (string_split '.' s))
+	    with Failure "int_of_string" -> raise (ContentError ("Unknown OId"))
+	  end
+	end
+	| _ -> raise (ContentError ("OId must be strings or int list"))
+      in _get_extension exts oid
+
+
+let test_usage ext_id test_fun cert =
+  match _get_content_extension (pop_option cert.tbs.extensions []) ext_id with
+    | V_List usages -> Some (test_fun (List.map eval_as_string usages))
+    | V_Unit -> None
+    | _ -> Some false
+
+let rec multimem pattern = function
+  | [] -> false
+  | x::r -> List.mem x pattern || multimem pattern r
+
+let raw_has_key_usage usage cert = test_usage keyUsage_oid (List.mem usage) cert
+let raw_has_cert_type cert_type cert = test_usage nsCertType_oid (List.mem cert_type) cert
+let raw_has_one_of_key_usages usages cert = test_usage keyUsage_oid (multimem usages) cert
+let raw_has_one_of_cert_types types cert = test_usage nsCertType_oid (multimem types) cert
+
+
+let raw_is_ca cert =
+  match _get_content_extension (pop_option cert.tbs.extensions []) basicConstraints_oid with
+    | V_Dict bc -> Some (eval_as_bool (hash_find_default bc "CA" (V_Bool false)))
+    | _ -> None
+
+
+let lift_test_ext f cert def_if_not_v3 def_if_ext_absent =
+  match cert.tbs.version with
+    | None | Some 1 | Some 2 -> V_Bool def_if_not_v3
+    | Some _ ->
+      match f cert with
+	| None -> V_Bool def_if_ext_absent
+	| Some b -> V_Bool b
+
+type blurry_result =
+  | Yes
+  | MostlyYes
+  | MostlyNo
+  | No
+
+
+let is_ca cert =
+  match cert.tbs.version with
+    | None | Some 1 | Some 2 -> Yes
+    | Some _ ->
+      match raw_is_ca cert, raw_has_key_usage "keyCertSign" cert,
+	raw_has_one_of_cert_types ["sslCA"; "emailCA"; "objCA"] cert with
+	  | Some true, (Some true | None), (Some true | None) -> Yes
+	  | Some true, _, _ -> MostlyYes
+	  | Some false, (Some false | None), (Some false | None) -> No
+	  | Some false, _, _ -> MostlyNo
+	  | None, (Some false | None), (Some false | None) -> No
+	  | None, (Some true | None), (Some true | None) -> MostlyYes
+	  | None, Some _, Some _ -> MostlyNo
+
+let string_of_blurry = function
+  | Yes -> V_String "Yes"
+  | MostlyYes -> V_String "MostlyYes"
+  | MostlyNo -> V_String "MostlyNo"
+  | No -> V_String "No"
+
 
 module X509 = struct
   type t = certificate
@@ -237,11 +306,8 @@ module X509 = struct
 	| None -> Hashtbl.replace dict "effective_version" (V_Int 1)
 	| Some v -> Hashtbl.replace dict "effective_version" (V_Int v)
     end;
-    begin
-      match cert.tbs.extensions with
-	| None -> Hashtbl.replace dict "get_extension" (V_Function (NativeFun (function _ -> V_Unit)))
-	| Some exts -> Hashtbl.replace dict "get_extension" (V_Function (NativeFun (one_value_fun (get_extension exts))))
-    end
+    Hashtbl.replace dict "get_extension" (V_Function (NativeFun (one_value_fun (get_extension cert.tbs.extensions))));
+    Hashtbl.replace dict "is_ca" (V_Function (NativeFun (zero_value_fun (fun () -> string_of_blurry (is_ca cert)))))
 
   let update dict = raise (NotImplemented "x509.update")
 
