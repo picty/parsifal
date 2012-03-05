7880d8748257585e6537572e94ed467d586a3d90
Olivier Levillain <olivier.levillain@picty.org>
2012-03-05 23:27:40 +0100

    Bugfix: answerDump ng did not work...

--- formats/answerDump.ml
+++ formats/answerDump.ml
@@ -1,3 +1,4 @@
+open Common
 open Types
 open ParsingEngine
 open Modules
@@ -8,10 +9,10 @@ module AnswerDump = struct
   let description = BinaryRecord.mk_desc [
     "ip", parse_ipv4, eval_as_ipv4, None;
     "port", parse_uint16, dumpv_uint16, None;
-    "name", parse_varlen_string pop_uint16, eval_as_string, None;
+    "name", parse_varlen_string pop_uint16, dump_varlen_string dump_uint16, None;
     "client_hello_type", parse_uint8, dumpv_uint8, None;
     "msg_type", parse_uint8, dumpv_uint8, None;
-    "content", parse_varlen_bin_string pop_uint32, eval_as_string, None
+    "content", parse_varlen_bin_string pop_uint32, dump_varlen_string dump_uint32, None
   ]
 end
 
