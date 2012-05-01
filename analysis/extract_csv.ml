27b4266f42cd7f2926586b96182491893eeb5e53
Olivier Levillain <olivier.levillain@picty.org>
2012-05-02 00:40:22 +0200

    Rewrote extract_csv to merge AnswerType, Alert, ServerHello and ServerHelloExtensions tables. The program also takes acceptable versions/suites/extensions into account from the client hello.

--- analysis/extract_csv.ml
+++ analysis/extract_csv.ml
@@ -1,4 +1,5 @@
 open Common
+open Base64
 open Types
 open ParsingEngine
 open BinaryRecord
@@ -8,6 +9,19 @@ open TlsCommon
 open TlsRecord
 open TlsHandshake
 open TlsAlert
+
+
+(*
+CLIENTHELLO_PATH=~/dev/ssl-campaigns/client-hellos CAMPAIGN_PATH=/media/disk/Campaigns ANALYSIS_PATH=/media/disk/Analysis-ng
+for campaign in 00{0,1,2,3,4,5,6,7,8,9}; do
+  echo "Working on campaign $campaign"
+  export ClientHello="$(openssl enc -base64 -in "$CLIENTHELLO_PATH/$campaign.client_hello")"
+  for i in $(seq 0 255); do
+  echo -n "  $i... "
+  ./extract_csv.native 2> /dev/null < "$CAMPAIGN_PATH/$campaign"*/"$i.dump" | ./split_data.py "$ANALYSIS_PATH/$campaign/$i-%s.txt"
+  echo done
+done; done
+*)
 
 
 let dhe_suites =
@@ -40,6 +54,7 @@ type msg =
   | CertificateRequest
   | ServerHelloDone
   | Other_HS of int
+  | ToBeIgnored
 
 
 let mk_uid answer answer_number =
@@ -53,24 +68,8 @@ let mk_hash s =
   String.sub (hexdump (Crypto.sha1sum s)) 0 16
 
 
-(* let string_of_type = function *)
-(*   | Junk -> "Junk" *)
-(*   | Alert (al, at) -> *)
-(*     "A(" ^ (string_of_int al) ^ "," ^ (string_of_int at) ^ ")" *)
-(*   | ServerHello (v, cs, exts) -> *)
-(*     "SH(" ^ (hexdump_int_n 4 v) ^ "," ^ (hexdump_int_n 4 cs) ^ ",{" ^ *)
-(*       (String.concat ";" (List.map string_of_int exts)) ^  "})" *)
-(*   | Certificates certs -> "Certs(" ^ (string_of_int (List.length certs)) ^ ")" *)
-(*   | CertificateRequest -> "CertReq" *)
-(*   | DHE_SKE (_, _, _) -> "SKE(DHE)" *)
-(*   | ECDHE_SKE _ -> "SKE(ECDHE)" *)
-(*   | Other_SKE _ -> "SKE" *)
-(*   | ServerHelloDone -> "SHD" *)
-(*   | Other_HS hs_type -> "HS" ^ (string_of_int hs_type) *)
-
-
-let get_type cs shdone record =
-  if !shdone then Junk else begin
+let get_type cs parsing_ended record =
+  if !parsing_ended then ToBeIgnored else begin
     let msg_type = record.content_type in
     match msg_type with
       | 22 -> begin
@@ -99,29 +98,18 @@ let get_type cs shdone record =
 		  else Other_SKE ""
 	    end
 	    | 13 -> CertificateRequest
-	    | 14 -> shdone := true; ServerHelloDone
+	    | 14 -> parsing_ended := true; ServerHelloDone
 	    | _ -> Other_HS hs_t
-	with _ -> shdone := true; Junk
+	with _ -> parsing_ended := true; Junk
       end
       | 21 -> begin
 	try
 	  let (al, at) = AlertModule.pop_object record.content in
 	  Alert (record.version, al, at)
-	with _ -> shdone := true; Junk
+	with _ -> parsing_ended := true; Junk
       end
-      | _ -> shdone := true; Junk
+      | _ -> parsing_ended := true; Junk
   end
-
-
-let print_alert uid v al at =
-  Printf.printf "AnswerTypes:%s:A\n" uid;
-  Printf.printf "Alerts:%s:%4.4x:%d:%d\n" uid v al at
-
-
-let print_serverhello ssl2 uid ext_v v cs exts =
-  Printf.printf "AnswerTypes:%s:%s\n" uid (if ssl2 then "2" else "H");
-  Printf.printf "ServerHellos:%s:%4.4x:%4.4x:%4.4x\n" uid ext_v v cs;
-  List.iter (fun e -> Printf.printf "ServerHelloExtensions:%s:%d\n" uid e) exts
 
 
 let rec print_hsmsg uid = function
@@ -141,13 +129,79 @@ let rec print_hsmsg uid = function
   | _ -> ()
 
 
-let _ =
-  let pstate = pstate_of_channel "(stdin)" stdin in
 
+exception WrongTLSAnswer
+
+let rec acceptable_versions min max = match min, max with
+  | 0x0002, 0x0002 -> [0x0002]
+  | 0x0002, _ -> 0x0002::(acceptable_versions 0x0300 max)
+  | _ ->
+    if min = max
+    then [min]
+    else min::(acceptable_versions (min+1) max)
+
+let handle_rfc5746 cs exts =
+  let rec aux cs_accu exts_accu = function
+    | [] -> List.rev cs_accu, exts_accu
+    | x::r ->
+      if x = 0x00ff
+      then aux cs_accu (65281::exts_accu) r
+      else aux (x::cs_accu) exts_accu r
+  in aux [] exts cs
+
+
+let describe_ch s =
+  let pstate = pstate_of_string None s in
+  try
+    let records, error = Tls.TlsLib.shallow_parse_records pstate in
+    let parsed_recs = Tls.TlsLib._deep_parse_aux "client_hello" records true in
+    match parsed_recs with
+      | [{content_type = 0x16; content = c}] ->
+	begin
+	  match HandshakeModule.pop_object c with
+	    | 1, V_Dict ch ->
+	      let vmax = eval_as_int (ch --> "version")
+	      and ciphersuites = List.map eval_as_int (eval_as_list (ch --> "cipher_suites"))
+	      and extensions = match (ch --> "extensions") with
+		| V_List l -> List.map (fun x -> eval_as_int (List.hd (eval_as_list x))) l
+		| _ -> []
+	      in
+	      let real_cs, real_exts = handle_rfc5746 ciphersuites extensions in
+	      (acceptable_versions 0x0300 vmax, real_cs, real_exts)
+	    | _ -> raise WrongTLSAnswer
+	end
+      | _ -> raise WrongTLSAnswer
+  with WrongTLSAnswer ->
+    let pstate = pstate_of_string None s in
+    match Ssl2.parse pstate with
+      | Ssl2.ClientHello (vmax, ciphersuites, _sid, _challenge) ->
+	let real_cs, real_exts = handle_rfc5746 ciphersuites [] in
+	(acceptable_versions 0x0002 vmax, real_cs, real_exts)
+      | _ -> raise WrongTLSAnswer
+
+
+let mk_hs_type version_ok cs_ok ext_ok =
+  match version_ok && cs_ok && ext_ok with
+    | true -> "H"
+    | false ->
+      let res = String.copy "h(vce)" in
+      if version_ok then res.[2] <- 'V';
+      if cs_ok then res.[3] <- 'C';
+      if ext_ok then res.[4] <- 'E';
+      res
+
+
+
+let _ =
   TlsHandshake.parse_certificates := false;
   tolerance := s_specfatallyviolated;
   minDisplay := s_specfatallyviolated;
-  
+
+  let client_hello = from_base64 None (Sys.getenv "ClientHello") in
+  let existing_versions = acceptable_versions 0x0002 0x0303 in
+  let compatible_versions, acceptable_ciphersuites, acceptable_extensions = describe_ch client_hello in
+
+  let pstate = pstate_of_channel "(stdin)" stdin in
   try
     let answer_number = ref 0 in
     while not (eos pstate) do
@@ -157,33 +211,45 @@ let _ =
       let name = eval_as_string (answer --> "name") in
       if String.length name > 0
       then Printf.printf "Names:%s:%s\n" uid name;
-      
-      let tls_pstate = pstate_of_string (Some name) (eval_as_string (answer --> "content")) in
+
+      let content = eval_as_string (answer --> "content") in
+      let tls_pstate = pstate_of_string (Some name) content in
       let records, error = Tls.TlsLib.shallow_parse_records tls_pstate in
       let parsed_recs = Tls.TlsLib._deep_parse_aux name records true in
 
-      let cs = ref None and shdone = ref false in
-      let content_types = List.filter (fun x -> x != Junk && x != ServerHelloDone) (List.map (get_type cs shdone) parsed_recs) in
+      let cs = ref None and parsing_ended = ref false in
+      let content_types = List.filter (fun x -> x != ToBeIgnored && x != ServerHelloDone) (List.map (get_type cs parsing_ended) parsed_recs) in
 
       begin
 	match content_types with
-	  | (Alert (version, al, at))::_ -> print_alert uid version al at;
-	  | (ServerHello (external_version, version, cs, exts))::r ->
-	    print_serverhello false uid external_version version cs exts;
+	  | [Alert (version, al, at)] ->
+	    if List.mem version existing_versions
+	    then Printf.printf "AnswerTypes:%s:A:%4.4x:%d:%d\n" uid version al at
+	    else Printf.printf "AnswerTypes:%s:J\n" uid
+	  | (ServerHello (_, version, cs, exts))::r ->
+	    let version_ok = List.mem version compatible_versions
+	    and cs_ok = List.mem cs acceptable_ciphersuites
+	    and ext_ok = List.fold_left ( && ) true (List.map (fun e -> List.mem e acceptable_extensions) exts) in
+	    let t = mk_hs_type version_ok cs_ok ext_ok in
+	    let ext_str = String.concat "," (List.map string_of_int exts) in
+	    Printf.printf "AnswerTypes:%s:%s:%4.4x:%4.4x:%s\n" uid t version cs ext_str;
 	    print_hsmsg uid r
 	  | _ ->
 	    begin
-	      let ssl2_pstate = pstate_of_string (Some name) (eval_as_string (answer --> "content")) in
-	      let res = 
+	      let ssl2_pstate = pstate_of_string (Some name) content in
+	      let res =
 		try Some (Ssl2.parse ssl2_pstate)
 		with _ -> None
 	      in
 	      match res with
-		| Some (Ssl2.Error err) -> print_alert uid 0x0002 (err lsr 8) (err land 0xff)
+		| Some (Ssl2.Error err) -> Printf.printf "AnswerTypes:%s:A:0002:%d:%d\n" uid (err lsr 8) (err land 0xff)
 		| Some (Ssl2.ServerHello (_session_id_hit, _cert_type, v, cert, cs::_, _connection_id)) ->
-		  print_serverhello true uid 0x0002 v cs [];
-		  print_hsmsg uid [Certificates [cert]];
-		| _ -> Printf.printf "AnswerTypes:%s:J\n" uid;
+		  let version_ok = List.mem 0x0002 compatible_versions
+		  and cs_ok = List.mem cs acceptable_ciphersuites in
+		  let t = mk_hs_type version_ok cs_ok true in
+		  Printf.printf "AnswerTypes:%s:%s:%4.4x:%4.4x:\n" uid t v cs;
+		  print_hsmsg uid [Certificates [cert]]
+		| _ -> Printf.printf "AnswerTypes:%s:%c\n" uid (if content = "" then 'E' else 'J')
 	    end
       end
     done
