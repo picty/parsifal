8e8d9c98e3929e3efbec70c496e012a53c001e47
Olivier Levillain <olivier.levillain@picty.org>
2012-05-07 00:07:31 +0200

    Added handshake decapsulation.

--- ng/test_tls_record.ml
+++ ng/test_tls_record.ml
@@ -11,7 +11,12 @@ let _ =
   Printf.printf "TLS Record\n";
   Printf.printf "  Content type: %s\n" (string_of_tls_content_type tls_record.content_type);
   Printf.printf "  Version: %s\n" (string_of_tls_version tls_record.record_version);
-  Printf.printf "  Content length: %d\n" (String.length tls_record.content);
+  Printf.printf "  Content length: %d\n" (String.length tls_record.record_content);
+  if tls_record.content_type = RT_Handshake then begin
+    let hs_msg = parse_handshake_msg (input_of_string "Handshake" tls_record.record_content) in
+    Printf.printf "    Handshake message type: %s\n" (string_of_hs_message_type hs_msg.handshake_type);
+    Printf.printf "    Handshake content length: %d\n" (String.length hs_msg.handshake_content);
+  end;
   if dump_tls_record tls_record = s
   then print_endline "Yes!"
   else print_endline "NO!"
