bd707e77eb4c2566dd807fe5793a892e0267f3b9
Olivier Levillain <olivier.levillain@picty.org>
2012-06-22 17:56:27 +0200

    Added support for more generic Lwt_io channels not only Lwt_unix file descriptors.

--- ng/lwtParsingEngine.ml
+++ ng/lwtParsingEngine.ml
@@ -2,7 +2,7 @@ open Lwt
 open ParsingEngine
 
 type lwt_input = {
-  fd : Lwt_unix.file_descr;
+  lwt_ch : Lwt_io.input_channel;
   lwt_name : string;
   mutable lwt_offset : int;
 }
@@ -11,25 +11,27 @@ exception LwtOutOfBounds
 
 let input_of_fd name fd =
   {
-    fd = fd;
+    lwt_ch = Lwt_io.of_fd Lwt_io.input fd;
     lwt_name = name;
     lwt_offset = 0;
   }
 
-let really_read fd len =
+let input_of_channel name ch =
+  {
+    lwt_ch = ch;
+    lwt_name = name;
+    lwt_offset = 0;
+  }
+
+let really_read ch len =
   let buf = String.make len ' ' in
-  let rec do_read offset =
-    Lwt_unix.read fd buf offset (len - offset) >>= fun l ->
-    if l > 0 then begin
-      let new_offset = offset + l in
-      if new_offset = len
-      then return buf
-      else do_read new_offset
-    end else fail LwtOutOfBounds
-  in do_read 0
+  try
+    Lwt_io.read_into_exactly ch buf 0 len >>= fun () ->
+    return buf
+  with End_of_file -> fail LwtOutOfBounds
 
 let get_in input name len =
-  really_read input.fd len >>= fun s ->
+  really_read input.lwt_ch len >>= fun s ->
   return {
     str = s;
     cur_name = name;
@@ -48,32 +50,32 @@ let get_out old_input input =
 (* Integer parsing *)
 
 let lwt_parse_uint8 input =
-  really_read input.fd 1 >>= fun s ->
+  really_read input.lwt_ch 1 >>= fun s ->
   let res = int_of_char (s.[0]) in
   input.lwt_offset <- input.lwt_offset + 1;
   return res
 
 let lwt_parse_char input =
-  really_read input.fd 1 >>= fun s ->
+  really_read input.lwt_ch 1 >>= fun s ->
   let res = s.[0] in
   input.lwt_offset <- input.lwt_offset + 1;
   return res
 
 let lwt_parse_uint16 input =
-  really_read input.fd 2 >>= fun s ->
+  really_read input.lwt_ch 2 >>= fun s ->
   let res = ((int_of_char s.[0]) lsl 8) lor (int_of_char s.[1]) in
   input.lwt_offset <- input.lwt_offset + 2;
   return res
 
 let lwt_parse_uint24 input =
-  really_read input.fd 3 >>= fun s ->
+  really_read input.lwt_ch 3 >>= fun s ->
   let res = ((int_of_char s.[0]) lsl 16) lor
     ((int_of_char s.[1]) lsl 8) lor (int_of_char s.[2]) in
   input.lwt_offset <- input.lwt_offset + 3;
   return res
 
 let lwt_parse_uint32 input =
-  really_read input.fd 4 >>= fun s ->
+  really_read input.lwt_ch 4 >>= fun s ->
   let res = ((int_of_char s.[0]) lsl 24) lor ((int_of_char s.[1]) lsl 16)
     lor ((int_of_char s.[2]) lsl 8) lor (int_of_char s.[3]) in
   input.lwt_offset <- input.lwt_offset + 4;
@@ -84,7 +86,7 @@ let lwt_parse_uint32 input =
 (* String parsing *)
 
 let lwt_parse_string n input =
-  really_read input.fd n >>= fun s ->
+  really_read input.lwt_ch n >>= fun s ->
   input.lwt_offset <- input.lwt_offset + n;
   return s
 
@@ -96,7 +98,7 @@ let lwt_parse_varlen_string name len_fun input =
   return res
 
 let lwt_drop_bytes n input =
-  really_read input.fd n >>= fun _ ->
+  really_read input.lwt_ch n >>= fun _ ->
   input.lwt_offset <- input.lwt_offset + n;
   return ()
 
