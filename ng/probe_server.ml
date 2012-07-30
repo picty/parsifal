bcee7ca0b5c3e67be65e836ab52e1380ce0617f3
Olivier Levillain <olivier.levillain@picty.org>
2012-07-30 18:55:10 +0200

    Added options with getopt. probe_server is starting to be a cool tool.

--- ng/probe_server.ml
+++ ng/probe_server.ml
@@ -1,6 +1,7 @@
 open Lwt
 open Lwt_io
 open Unix
+open Getopt
 
 open ParsingEngine
 open DumpingEngine
@@ -11,10 +12,63 @@ open Tls
 open TlsEngine
 
 
-let mk_client_hello v cs ?extensions:(exts=None) =
+(* TODO:
+   - Add a --retry option
+   - Handle errors more smoothly
+   - Add extensions
+*)
+
+
+let verbose = ref false
+let chunk_size = ref 1400
+let host = ref "www.google.com"
+let port = ref 443
+let rec_version = ref V_TLSv1
+let ch_version = ref V_TLSv1
+let suites = ref ["TLS_RSA_WITH_RC4_128_SHA"]
+let timeout = ref 3.0
+
+let clear_suites () = suites := []; ActionDone
+let all_suites () =
+  let rec aux accu = function
+    | 0x10000 -> accu
+    | n -> begin
+      match ciphersuite_of_int n with
+	| TLS_UnknownSuite _ -> aux accu (n+1)
+	| _ -> aux ((string_of_int n)::accu) (n+1)
+    end
+  in
+  suites := aux [] 0;
+  ActionDone
+
+let update_version r s = r := tls_version_of_string s; ActionDone
+let update_both_versions s =
+  ignore (update_version rec_version s);
+  update_version ch_version s
+
+let set_timeout s = timeout := (float_of_string s); ActionDone
+
+let options = [
+  mkopt (Some 'h') "help" Usage "show this help";
+  mkopt (Some 'v') "version" (Set verbose) "print more info to stderr";
+  mkopt (Some 'H') "host" (StringVal host) "host to contact";
+  mkopt (Some 'p') "port" (IntVal port) "port to probe";
+  mkopt (Some 'V') "version" (StringFun update_both_versions) "sets the record and ClientHello versions";
+  mkopt None "record-version" (StringFun (update_version rec_version)) "sets the record versions";
+  mkopt None "client-hello-version" (StringFun (update_version ch_version)) "sets the ClientHello versions";
+  mkopt (Some 'C') "clear-suites" (SimpleFun clear_suites) "reset the list of suites";
+  mkopt (Some 'A') "add-suite" (StringList suites) "add a suite to the list of suites";
+  mkopt None "all-suites" (SimpleFun all_suites) "add all the suites imaginable";
+  mkopt None "record-size" (IntVal chunk_size) "sets the size of the records sent";
+  mkopt (Some 't') "timeout" (StringFun set_timeout) "sets the timeout";
+]
+
+
+
+let mk_client_hello ext_v v cs exts =
   {
     content_type = CT_Handshake;
-    record_version = V_TLSv1;
+    record_version = ext_v;
     record_content = Handshake {
       handshake_type = HT_ClientHello;
       handshake_content = ClientHello {
@@ -52,7 +106,7 @@ let handle_answer handle_hs handle_alert s =
     timed_read_answers ()
   and timed_read_answers () =
     let t = read_answers () in
-    pick [t; Lwt_unix.sleep 3.0 >>= fun () -> return None]
+    pick [t; Lwt_unix.sleep !timeout >>= fun () -> return None]
   in
 
   let rec parse_hs_msgs () =
@@ -94,7 +148,7 @@ let write_record_by_chunks o record size =
 
 
 let print_hs ctx hs =
-  print_endline (print_handshake_msg "" "Handshake" hs);
+  print_endline (print_handshake_msg "" "Handshake (S->C)" hs);
   match hs.handshake_type, hs.handshake_content with
   | HT_ServerHelloDone, _ -> Some ()
   | _, ServerHello { ciphersuite = cs } ->
@@ -110,7 +164,7 @@ let print_cs _ hs =
     | _ -> None
 
 let print_alert alert =
-  print_endline (print_tls_alert "" "Alert" alert);
+  print_endline (print_tls_alert "" "Alert (S->C)" alert);
   if alert.alert_level = AL_Fatal
   then Some ()
   else None
@@ -119,32 +173,31 @@ let do_nothing _ = None
 
 
 
-
-
-let remote_addr =
-  let host_entry = Unix.gethostbyname "dev.yeye.fr" in
+let mk_remote_addr host port =
+  let host_entry = Unix.gethostbyname host in
   let inet_addr = host_entry.Unix.h_addr_list.(0) in
-  Unix.ADDR_INET (inet_addr, 443)
+  Unix.ADDR_INET (inet_addr, port)
 
-let send_and_receive v cs addr hs_fun alert_fun =
+
+
+let send_and_receive ext_v v cs (host, port) hs_fun alert_fun =
   let s = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-  let exts = [
-    { extension_type = HE_EllipticCurves;
-      extension_data = Unparsed_HelloExtension "\x00\x32\x00\x0e\x00\x0d\x00\x19\x00\x0b\x00\x0c\x00\x18\x00\x09\x00\x0a\x00\x16\x00\x17\x00\x08\x00\x06\x00\x07\x00\x14\x00\x15\x00\x04\x00\x05\x00\x12\x00\x13\x00\x01\x00\x02\x00\x03\x00\x0f\x00\x10\xff\x01" }
-  ] in
-  let ch = mk_client_hello v cs ~extensions:(Some exts) in
+  let addr = mk_remote_addr host port in
   Lwt_unix.connect s addr >>= fun () ->
-  write_record_by_chunks s ch 1400 >>= fun () ->
+  if !verbose then Printf.fprintf Pervasives.stderr "Connected to %s:%d\n" host port;
+  let ch = mk_client_hello ext_v v cs None in
+  if !verbose then prerr_endline (print_tls_record "" "Sending Handshake (C->S)" ch);
+  write_record_by_chunks s ch !chunk_size >>= fun () ->
   handle_answer hs_fun alert_fun (input_of_fd "Server" s)
 
 
-let main_simple_probe addr suites =
-  send_and_receive V_TLSv1_2 suites addr print_hs print_alert
+let main_simple_probe addr ext_v v suites =
+  send_and_receive ext_v v suites addr print_hs print_alert
 
 
-let ssl_scan addr suites =
+let ssl_scan addr ext_v v suites =
   let rec next_step s =
-    send_and_receive V_TLSv1 s addr print_cs do_nothing >>= fun res ->
+    send_and_receive ext_v v s addr print_cs do_nothing >>= fun res ->
     match res with
       | None -> return []
       | Some suite_selected ->
@@ -158,6 +211,13 @@ let ssl_scan addr suites =
   in next_step suites
 
 
+
+
 let _ =
-  Lwt_unix.run (main_simple_probe remote_addr [TLS_RSA_EXPORT_WITH_RC4_40_MD5; TLS_RSA_WITH_RC4_128_SHA; TLS_DHE_RSA_WITH_AES_128_CBC_SHA])
-(*  Lwt_unix.run (ssl_scan remote_addr [TLS_RSA_EXPORT_WITH_RC4_40_MD5; TLS_RSA_WITH_RC4_128_SHA; TLS_DHE_RSA_WITH_AES_128_CBC_SHA; TLS_RSA_WITH_AES_256_CBC_SHA]) *)
+  let args = parse_args "probe_server" options Sys.argv in
+  let ciphersuites = List.map ciphersuite_of_string !suites in
+  let remote_addr = (!host, !port) in
+  match args with
+    | ["scan"] -> ignore (Lwt_unix.run (ssl_scan remote_addr !rec_version !ch_version ciphersuites))
+    | ["probe"] | [] -> ignore (Lwt_unix.run (main_simple_probe remote_addr !rec_version !ch_version ciphersuites))
+    | _ -> failwith "Invalid command"
