6d27a74433cd19647f12026dd2bca14fde1f1168
Olivier Levillain <olivier.levillain@picty.org>
2012-07-31 15:30:04 +0200

    Add support for compression scan in probe-server. Add a framework for new commands (with result sent): timeouts and end of files are better handled in particular.

--- ng/probe_server.ml
+++ ng/probe_server.ml
@@ -21,6 +21,13 @@ open TlsEngine
 
 (* Option handling *)
 
+type 'a result_type =
+  | NothingSoFar
+  | Result of 'a
+  | FatalAlert of string
+  | EndOfFile
+  | Timeout
+
 let verbose = ref false
 let chunk_size = ref 1400
 let host = ref "www.google.com"
@@ -28,6 +35,7 @@ let port = ref 443
 let rec_version = ref V_TLSv1
 let ch_version = ref V_TLSv1
 let suites = ref [TLS_RSA_WITH_RC4_128_SHA]
+let compressions = ref [CM_Null]
 let timeout = ref 3.0
 
 let clear_suites () = suites := []
@@ -48,24 +56,50 @@ let all_suites () =
   suites := aux [] 0
 let rev_suites () = suites := List.rev (!suites)
 
+let clear_compressions () = compressions := []
+let add_compression s =
+  try
+    compressions := (compression_method_of_string s)::(!compressions);
+    ActionDone
+  with _ -> ShowUsage (Some "Invalid compression method")
+let all_compressions () =
+  let rec aux accu = function
+    | 0x100 -> accu
+    | n -> begin
+      match compression_method_of_int n with
+	| CM_UnknownVal _ -> aux accu (n+1)
+	| cm -> aux (cm::accu) (n+1)
+    end
+  in
+  compressions := aux [] 0
+let rev_compressions () = compressions := List.rev (!compressions)
+
 let update_version r s = r := tls_version_of_string s; ActionDone
 let update_both_versions s =
-  ignore (update_version rec_version s);
-  update_version ch_version s
-
+  rec_version := tls_version_of_string s;
+  ch_version := tls_version_of_string s;
+  ActionDone
 
 let options = [
   mkopt (Some 'h') "help" Usage "show this help";
   mkopt (Some 'v') "verbose" (Set verbose) "print more info to stderr";
+
   mkopt (Some 'H') "host" (StringVal host) "host to contact";
   mkopt (Some 'p') "port" (IntVal port) "port to probe";
-  mkopt (Some 'V') "version" (StringFun update_both_versions) "sets the record and ClientHello versions";
-  mkopt None "record-version" (StringFun (update_version rec_version)) "sets the record versions";
-  mkopt None "client-hello-version" (StringFun (update_version ch_version)) "sets the ClientHello versions";
-  mkopt None "record-size" (IntVal chunk_size) "sets the size of the records sent";
+
+  mkopt (Some 'V') "version" (StringFun update_both_versions) "set the record and ClientHello versions";
+  mkopt None "record-version" (StringFun (update_version rec_version)) "set the record versions";
+  mkopt None "client-hello-version" (StringFun (update_version ch_version)) "set the ClientHello versions";
+
   mkopt (Some 'C') "clear-suites" (TrivialFun clear_suites) "reset the list of suites";
   mkopt (Some 'A') "add-suite" (StringFun add_suite) "add a suite to the list of suites";
   mkopt None "all-suites" (TrivialFun all_suites) "add all the known suites";
+
+  mkopt None "clear-compressions" (TrivialFun clear_compressions) "reset the list of compresion methods";
+  mkopt None "add-compression" (StringFun add_compression) "add a suite to the list of compresion methods";
+  mkopt None "all-compressions" (TrivialFun all_compressions) "add all the known compression methods";
+
+  mkopt None "record-size" (IntVal chunk_size) "set the size of the records sent";
   mkopt (Some 't') "timeout" (FloatVal timeout) "set the timeout";
 ]
 
@@ -90,16 +124,38 @@ let mk_client_hello exts =
 	client_random = String.make 32 '\x00';
 	client_session_id = "";
 	ciphersuites = !suites;
-	compression_methods = [CM_Null];
+	compression_methods = !compressions;
 	client_extensions = exts
       }
     }
   }
 
+let rec _really_write o s p l =
+  Lwt_unix.write o s p l >>= fun n ->
+  if l = n then
+    Lwt.return ()
+  else
+    _really_write o s (p + n) (l - n)
+
+let really_write o s = _really_write o s 0 (String.length s)
 
 let write_exactly o record_contet =
   let s = dump_record_content record_contet in
-  write_from_exactly  o s 0 (String.length s)
+  write_from_exactly o s 0 (String.length s)
+
+let write_record o record =
+  let s = dump_tls_record record in
+  really_write o s
+
+let write_record_by_chunks o record size =
+  let recs = TlsUtil.split_record record size in
+  Lwt_list.iter_s (write_record o) recs
+
+let catch_eof = function
+  | End_of_file -> return EndOfFile
+  | e -> fail e
+
+
 
 let handle_answer handle_hs handle_alert s =
   let ctx = TlsContext.empty_context () in
@@ -120,118 +176,113 @@ let handle_answer handle_hs handle_alert s =
     timed_read_answers ()
   and timed_read_answers () =
     let t = read_answers () in
-    pick [t; Lwt_unix.sleep !timeout >>= fun () -> return None]
+    pick [t; Lwt_unix.sleep !timeout >>= fun () -> return Timeout]
   in
 
   let rec parse_hs_msgs () =
     lwt_parse_handshake_msg ~context:(Some ctx) hs_lwt_in >>= fun hs_msg ->
     match handle_hs ctx hs_msg with
-      | None -> parse_hs_msgs ()
-      | Some x -> return (Some x)
+      | NothingSoFar -> parse_hs_msgs ()
+      | x -> return x
   in
 
   let rec parse_alert_msgs () =
     lwt_parse_tls_alert alert_lwt_in >>= fun alert ->
     match handle_alert alert with
-      | None -> parse_alert_msgs ()
-      | Some x -> return (Some x)
+      | NothingSoFar -> parse_alert_msgs ()
+      | x -> return x
   in
 
   let p1 = parse_hs_msgs ()
   and p2 = parse_alert_msgs () in
-  pick [p1; p2; timed_read_answers ()]
+  catch (fun () -> pick [p1; p2; timed_read_answers ()]) catch_eof
 
-
-let rec _really_write o s p l =
-  Lwt_unix.write o s p l >>= fun n ->
-  if l = n then
-    Lwt.return ()
-  else
-    _really_write o s (p + n) (l - n)
-
-let really_write o s = _really_write o s 0 (String.length s)
-
-
-let write_record o record =
-  let s = dump_tls_record record in
-  really_write o s
-
-let write_record_by_chunks o record size =
-  let recs = TlsUtil.split_record record size in
-  Lwt_list.iter_s (write_record o) recs
 
 
 let print_hs ctx hs =
   print_endline (print_handshake_msg "" "Handshake (S->C)" hs);
   match hs.handshake_type, hs.handshake_content with
-  | HT_ServerHelloDone, _ -> Some ()
+  | HT_ServerHelloDone, _ -> Result ()
   | _, ServerHello { ciphersuite = cs } ->
     ctx.future.s_ciphersuite <- cs;
-    None
-  | _ -> None
+    NothingSoFar
+  | _ -> NothingSoFar
 
-let print_cs _ hs =
+let get_cs _ hs =
   match hs.handshake_content with
-    | ServerHello { ciphersuite = cs } ->
-      print_endline (string_of_ciphersuite cs);
-      Some cs
-    | _ -> None
+    | ServerHello { ciphersuite = cs } -> Result cs
+    | _ -> NothingSoFar
+
+let get_cm _ hs =
+  match hs.handshake_content with
+    | ServerHello { compression_method = cm } -> Result cm
+    | _ -> NothingSoFar
+
+let stop_on_fatal_alert alert =
+  if alert.alert_level = AL_Fatal
+  then FatalAlert (string_of_tls_alert_type alert.alert_type)
+  else NothingSoFar
 
 let print_alert alert =
   print_endline (print_tls_alert "" "Alert (S->C)" alert);
   if alert.alert_level = AL_Fatal
-  then Some ()
-  else None
+  then FatalAlert (string_of_tls_alert_type alert.alert_type)
+  else NothingSoFar
 
-let do_nothing _ = None
-
-
-
-let mk_remote_addr host port =
-  let host_entry = Unix.gethostbyname host in
-  let inet_addr = host_entry.Unix.h_addr_list.(0) in
-  Unix.ADDR_INET (inet_addr, port)
+let do_nothing _ = NothingSoFar
 
 
 
-let send_and_receive (host, port) hs_fun alert_fun =
+let send_and_receive hs_fun alert_fun =
   let s = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-  let addr = mk_remote_addr host port in
+  let host_entry = Unix.gethostbyname !host in
+  let inet_addr = host_entry.Unix.h_addr_list.(0) in
+  let addr = Unix.ADDR_INET (inet_addr, !port) in
   Lwt_unix.connect s addr >>= fun () ->
-  if !verbose then Printf.fprintf Pervasives.stderr "Connected to %s:%d\n" host port;
+  if !verbose then Printf.fprintf Pervasives.stderr "Connected to %s:%d\n" !host !port;
   let ch = mk_client_hello None in
   if !verbose then prerr_endline (print_tls_record "" "Sending Handshake (C->S)" ch);
   write_record_by_chunks s ch !chunk_size >>= fun () ->
   handle_answer hs_fun alert_fun (input_of_fd "Server" s)
 
 
-let main_simple_probe addr =
-  send_and_receive addr print_hs print_alert
 
+let remove_from_list list elt =
+  list := List.filter (fun x -> x <> elt) !list;
+  !list <> []
 
-let ssl_scan addr =
+let ssl_scan get to_string update =
   let rec next_step () =
-    send_and_receive addr print_cs do_nothing >>= fun res ->
+    send_and_receive get stop_on_fatal_alert >>= fun res ->
     match res with
-      | None -> return []
-      | Some suite_selected ->
-	let next_suites = List.filter (fun x -> x <> suite_selected) !suites in
-	if next_suites = []
-	then return [suite_selected]
-	else begin
-	  suites := next_suites;
-	  next_step () >>= fun r ->
-	  return (suite_selected::r)
-	end
+      | Result r ->
+	if update r then begin
+	  next_step () >>= fun other_res ->
+	  match other_res with
+	    | Result others -> return (Result ((to_string r)::others))
+	    | _ -> return (Result [to_string r])
+	end else return (Result [to_string r])
+      | NothingSoFar -> return (Result [])
+      | Timeout -> return (Result ["Timeout"])
+      | EndOfFile -> return (Result ["EndOfFile"])
+      | FatalAlert s -> return (Result ["FatalAlert \"" ^ s ^ "\""])
   in next_step ()
 
+let print_list = List.iter print_endline
 
+let print_result print_fun = function
+  | Result r -> print_fun r
+  | NothingSoFar -> prerr_endline "NothingSoFar"
+  | Timeout -> prerr_endline "Timeout"
+  | EndOfFile -> prerr_endline "EndOfFile"
+  | FatalAlert s -> prerr_endline ("FatalAlert \"" ^ s ^ "\"")
 
 
 let _ =
   let args = parse_args getopt_params Sys.argv in
-  let remote_addr = (!host, !port) in
   match args with
-    | ["scan"] -> ignore (Lwt_unix.run (ssl_scan remote_addr))
-    | ["probe"] | [] -> ignore (Lwt_unix.run (main_simple_probe remote_addr))
+    | ["scan-suites"] -> print_result print_list (Lwt_unix.run (ssl_scan get_cs string_of_ciphersuite (remove_from_list suites)))
+    | ["scan-compressions"] -> print_result print_list (Lwt_unix.run (ssl_scan get_cm string_of_compression_method (remove_from_list compressions)))
+(*    | ["version_scan"] -> Lwt_unix.run (ssl_scan ()) *)
+    | ["probe"] | [] -> print_result ignore (Lwt_unix.run (send_and_receive print_hs print_alert))
     | _ -> failwith "Invalid command"
