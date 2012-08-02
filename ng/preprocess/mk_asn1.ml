09a36d06a958d0e65cc0fdcc958fcbdf0344f95d
Olivier Levillain <olivier.levillain@picty.org>
2012-08-03 00:48:46 +0200

    Little rewriting of hd/mk_asn1 to handle a desc on-the-fly instead of handling all of them at the end.

--- ng/preprocess/mk_asn1.ml
+++ ng/preprocess/mk_asn1.ml
@@ -1,3 +1,54 @@
+type expected_header_string = string option
+
+type field_type =
+  | AT_Boolean
+  | AT_SmallInteger
+  | AT_Integer
+  | AT_BitString
+  (*  TODO: AT_EnumeratedBitString *)
+  | AT_Null
+  | AT_OId
+  (* AT_String (constraints : None -> no_constraint, Some s -> $s_constraint) *)
+  | AT_String of string option
+  | AT_Primitive
+  | AT_Container of expected_header_string * field_type
+  (* AT_SequenceOf (name of the sequence [default is proposed_name_list], min, max, sub_type, sub_header) *)
+  | AT_SequenceOf of string option * int option * int option * expected_header_string * field_type
+  | AT_SetOf of string option * int option * int option * expected_header_string * field_type
+
+  | AT_Custom of string option * string
+  | AT_Anything
+
+type asn1_option =
+  | AO_EnrichRawString
+  | AO_EnrichASN1Info
+  | AO_TopLevel
+
+(* TODO: Add constraints
+    - optional fields SHOULDs and SHOULDNOTs *)
+
+
+type field_desc = {
+  field_name : string;
+  field_type : field_type;
+  field_optional : bool;
+  field_expected_header : string option;
+}
+
+type description = {
+  name : string;
+  fields : field_desc list;
+  expected_header : string option;
+  options : asn1_option list;
+}
+
+
+let mkf ?opt:(o=false) ?hdr:(h=None) n t = {
+  field_name = n;
+  field_type = t;
+  field_optional = o;
+  field_expected_header = h;
+}
 
 
 (* Generic functions *)
@@ -177,13 +228,15 @@ let mk_parse_fun d =
   end
 
 
-let handle_desc (desc : description) =
+let handle_desc ?options:(o=[]) ?hdr:(h=None) n f =
+  let desc = {
+    name = n;
+    fields = f;
+    expected_header = h;
+    options = o;
+  } in
   mk_desc_type desc;
   mk_parse_fun desc;
-(*  if (List.mem AO_TopLevel desc.options) then begin
-    mk_exact_parse_fun desc
-    mk_lwt_parse_fun desc;
-  end; *)
   print_newline ()
 
 
@@ -191,6 +244,9 @@ let _ =
   print_endline "open Lwt";
   print_endline "open Asn1Enums";
   print_endline "open ParsingEngine";
-  print_endline "open Asn1Engine\n";
-  List.iter handle_desc descriptions
+  print_endline "open Asn1Engine\n";;
+
+
+
+(* Real information about ASN.1 structures goes after here. *)
 
