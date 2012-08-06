9bc51dfd3307a3c8c7ca0a758a313d1f4f9700d0
Olivier Levillain <olivier.levillain@picty.org>
2012-08-07 01:18:03 +0200

    More work on a camlp4 version of the pre-processing.
    It seems to work for enums.

--- ng/preprocess/mk_enum_camlp4.ml
+++ ng/preprocess/mk_enum_camlp4.ml
@@ -1,40 +1,124 @@
-(* ocamlc -pp "camlp4o pa_extend.cmo q_MLast.cmo" -I /usr/lib/ocaml/camlp4 -c preprocess_camlp4.ml *)
+(* ocamlc -pp "camlp4o pa_extend.cmo q_MLast.cmo" -I /usr/lib/ocaml/camlp4 -c mk_enum_camlp4.ml *)
 
 open Camlp4
 open Camlp4.PreCast
 open Syntax
 
-type ctor_name = string
-type choice = Loc.t * int * ctor_name * string
-
-type type_name = string
 type unknown_behaviour =
-  | DefaultVal of string
   | UnknownVal of string
   | Exception of string
-type enum = Loc.t * type_name * choice list * (Loc.t * unknown_behaviour)
 
 
 let ctor_name_of_ident _loc = function
   | <:ident< $uid:c$ >> -> c
   | _ -> Loc.raise _loc (Failure "constructor expected")
 
-let mk_ctors (_loc, name, enum, (loc_u, unknown)) =
-  let ctors = List.map (fun (_, _, n, _) -> n) enum in
+
+
+let mk_exception = function
+  | _loc, Exception e ->
+    let t = <:ctyp< $uid:e$ >> in
+    <:str_item< exception  $typ:t$  >>
+  | _loc, _ -> <:str_item< >>
+
+
+let mk_ctors (_loc, name, enum, unknown) =
+  let ctors = List.map (fun (_loc, _, n, _) -> <:ctyp< $uid:n$ >> ) enum in
 
   let suffix_choice = match unknown with
-    | DefaultVal v ->
-      if not (List.mem v ctors)
-      then Loc.raise loc_u (Failure "the default value is not specified")
-      else []
-    | UnknownVal v -> [v]
-    | Exception _ -> []
+    | _loc, UnknownVal v -> [ <:ctyp<  $ <:ctyp< $uid:v$ >> $  of  int  >> ]
+    | _ -> []
   in
 
-  let ctyp_of_uid c = <:ctyp< $uid:c$ >> in
-  let ctyp_ctors = List.map ctyp_of_uid (ctors@suffix_choice) in
+  let ctyp_ctors = ctors@suffix_choice in
   let constructors = Ast.tyOr_of_list ctyp_ctors in
   <:str_item< type $lid:name$ = $constructors$ >>
+
+
+let mk_string_of_enum (_loc, name, enum, unknown) =
+  let mk_case (_loc, _, n, d) =
+    let p, e = ( <:patt< $uid:n$ >>, <:expr< $str:d$ >> ) in
+    <:match_case< $p$ -> $e$ >>
+  in
+  let _cases = List.map mk_case enum in
+  let cases = match unknown with
+    | _loc, UnknownVal v ->
+      let p = <:patt<  $ <:patt< $uid:v$ >> $  $ <:patt< $lid:"i"$ >> $  >>
+      and e = <:expr< $str:"Unknown " ^ name ^ " ("$ ^ (string_of_int i) ^ $str:")"$ >>
+      in _cases@[ <:match_case< $p$ -> $e$ >> ]
+    | _ -> _cases
+  in
+  let body = <:expr< fun [ $list:cases$ ] >>
+  and fun_name = <:patt< $lid:("string_of_" ^ name)$ >> in
+  let bindings = <:binding< $pat:fun_name$ = $exp:body$ >> in
+  <:str_item< value $bindings$ >>
+
+
+let mk_int_of_enum (_loc, name, enum, unknown) =
+  let mk_case (_loc, v, n, _) =
+    let p, e = ( <:patt< $uid:n$ >>, <:expr< $int:v$ >> ) in
+    <:match_case< $p$ -> $e$ >>
+  in
+  let _cases = List.map mk_case enum in
+  let cases = match unknown with
+    | _loc, UnknownVal v ->
+      let p = <:patt<  $ <:patt< $uid:v$ >> $  $ <:patt< $lid:"i"$ >> $  >>
+      and e = <:expr< $lid:"i"$ >>
+      in _cases@[ <:match_case< $p$ -> $e$ >> ]
+    | _ -> _cases
+  in
+  let body = <:expr< fun [ $list:cases$ ] >>
+  and fun_name = <:patt< $lid:("int_of_" ^ name)$ >> in
+  let bindings = <:binding< $pat:fun_name$ = $exp:body$ >> in
+  <:str_item< value $bindings$ >>
+
+
+let mk_enum_of_int (_loc, name, enum, unknown) =
+  let mk_case (_loc, v, n, _) =
+    let p, e = ( <:patt< $int:v$ >>, <:expr< $uid:n$ >> ) in
+    <:match_case< $p$ -> $e$ >>
+  in
+  let _cases = List.map mk_case enum in
+  let cases = match unknown with
+    | _loc, UnknownVal v ->
+      let p = <:patt< $lid:"i"$ >>
+      and e = <:expr< $ <:expr< $uid:v$ >> $  $ <:expr< $lid:"i"$ >> $  >>
+      in _cases@[ <:match_case< $p$ -> $e$ >> ]
+    | _loc, Exception e ->
+      let p = <:patt< _ >>
+      and e = <:expr< $ <:expr< $lid:"raise"$ >> $  $ <:expr< $uid:e$ >> $  >>
+      in _cases@[ <:match_case< $p$ -> $e$ >> ]
+  in
+  let body = <:expr< fun [ $list:cases$ ] >>
+  and fun_name = <:patt< $lid:(name ^ "_of_int")$ >> in
+  let bindings = <:binding< $pat:fun_name$ = $exp:body$ >> in
+  <:str_item< value $bindings$ >>
+
+
+let mk_enum_of_string (_loc, name, enum, unknown) =
+  let mk_case (_loc, _, n, d) =
+    let p, e = ( <:patt< $str:d$ >>, <:expr< $uid:n$ >> ) in
+    <:match_case< $p$ -> $e$ >>
+  in
+  let _cases = List.map mk_case enum in
+  let cases =
+    let p = <:patt< $lid:"s"$ >>
+    and e = <:expr< $ <:expr< $lid:(name ^ "_of_int")$ >> $  ( $ <:expr< $lid:"int_of_string"$ >> $  $ <:expr< $uid:"s"$ >> $ )  >>
+    in _cases@[ <:match_case< $p$ -> $e$ >> ]
+  in
+  let body = <:expr< fun [ $list:cases$ ] >>
+  and fun_name = <:patt< $lid:(name ^ "_of_string")$ >> in
+  let bindings = <:binding< $pat:fun_name$ = $exp:body$ >> in
+  <:str_item< value $bindings$ >>
+
+
+
+(* let mk_parse_dump_print_funs do_lwt (name, _, _) = *)
+(*   Printf.printf "let parse_%s parse_int input = %s_of_int (parse_int input)\n" name name; *)
+(*   if do_lwt *)
+(*   then Printf.printf "let lwt_parse_%s lwt_parse_int input = lwt_parse_int input >>= fun x -> return (%s_of_int x)\n" name name; *)
+(*   Printf.printf "let dump_%s dump_int v = dump_int (int_of_%s v)\n" name name; *)
+(*   Printf.printf "let print_%s = print_enum string_of_%s int_of_%s\n" name name name *)
 
 
 
@@ -43,28 +127,31 @@ EXTEND Gram
 
   enum_elts: [ [
     e1 = SELF; ";"; e2 = SELF -> e1 @ e2
-  | v = INT; ":"; constructor = ident; ":"; display = STRING ->
-    [(_loc, int_of_string v, ctor_name_of_ident _loc constructor, display)]
+  | v = INT; ","; constructor = ident; ","; display = STRING ->
+    [(_loc, v, ctor_name_of_ident _loc constructor, display)]
   | -> []
   ] ];
 
 
   unknown_behaviour: [[
     "Exception"; x = ident -> _loc, Exception (ctor_name_of_ident _loc x)
-  | "DefaultVal"; x = ident -> _loc, DefaultVal (ctor_name_of_ident _loc x)
   | "UnknownVal"; x = ident -> _loc, UnknownVal (ctor_name_of_ident _loc x)
   ]];
 
   str_item: [[
-    "enum"; enum_name = ident; "="; "{"; choices = enum_elts; "}";
-              ","; "<"; u_b = unknown_behaviour; ">" ->
+    "enum"; enum_name = ident; "="; "["; choices = enum_elts; "]";
+              ","; "["; u_b = unknown_behaviour; "]" ->
       let n = match enum_name with
         | <:ident< $lid:nn$ >> -> nn
         | _ -> Loc.raise _loc (Failure "type name expected")
       in
-      mk_ctors (_loc, n, choices, u_b)
-    (* let si1 = <:str_item< type $typ:enum_name$ = unit >> in *)
-    (* <:str_item< declare $si1$; $si1$; end >> ]]; *)
+      let si0 = mk_exception u_b
+      and si1 = mk_ctors (_loc, n, choices, u_b)
+      and si2 = mk_string_of_enum (_loc, n, choices, u_b)
+      and si3 = mk_int_of_enum (_loc, n, choices, u_b)
+      and si4 = mk_enum_of_int (_loc, n, choices, u_b)
+      and si5 = mk_enum_of_string (_loc, n, choices, u_b) in
+      <:str_item< $si0$; $si1$; $si2$; $si3$; $si4$; $si5$ >>
   ]];
 END
 ;;
--- ng/preprocess/mk_enum_camlp4_test.ml
+++ ng/preprocess/mk_enum_camlp4_test.ml
@@ -1,10 +1,12 @@
-(* ocamlc -pp "camlp4o ./preprocess_camlp4.cmo" preprocess_camlp4_test.ml *)
+(* ocamlc -pp "camlp4o ./preprocess_camlp4.cmo" mk_enum_camlp4_test.ml *)
 
-enum tls_version = {
-  0x0002: V_SSLv2: "SSLv2";
-  0x0300: V_SSLv3: "SSLv3";
-  0x0301: V_TLSv1: "TLSv1.0";
-  0x0302: V_TLSv1_1: "TLSv1.1";
-  0x0303: V_TLSv1_2: "TLSv1.2";
-}, <UnknownVal V_Unknown>
+enum tls_version = [
+  0x0002, V_SSLv2, "SSLv2";
+  0x0300, V_SSLv3, "SSLv3";
+  0x0301, V_TLSv1, "TLSv1.0";
+  0x0302, V_TLSv1_1, "TLSv1.1";
+  0x0303, V_TLSv1_2, "TLSv1.2";
+], [UnknownVal V_Unknown]
 
+let _ =
+  print_endline (string_of_tls_version (tls_version_of_int 768));
