741c42f7357ea561b04a593e710dd0fd6a00ad00
Olivier Levillain <olivier.levillain@picty.org>
2012-08-07 15:18:52 +0200

    Removed the old enums preprocessor altogether. Now, tlsEnums are also preprocessed by camlp4.

--- ng/preprocess/hd_enums.ml
+++
@@ -1,9 +0,0 @@
-type ctor_name = string
-type choice = int * ctor_name * string
-
-type type_name = string
-type unknown_behaviour =
-  | DefaultVal of string
-  | UnknownVal of string
-  | Exception of string
-type enum = type_name * choice list * unknown_behaviour
