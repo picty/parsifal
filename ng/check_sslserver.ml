9238844cf596a8176cf788d29e48d626a0b87e25
Olivier Levillain <olivier.levillain@picty.org>
2012-08-16 14:47:40 +0200

    Cleaned-up the repository.
    The ng directory should become the main one shortly.

---
+++ ng/check_sslserver.ml
@@ -0,0 +1,73 @@
+(* check_sslserver: TODO
+
+Protocol Versions:
+ - check the accepted SSL version
+ - determine the preferred version (with different stimuli (SSLv2 CH, other TLS CH)
+ - check the version intolerance (for example a CH 0x03ff)
+ - check the coherency between record_version and clienthello_version
+ - check the tolerance to various (record_version/clienthello_version)
+ - check the consistency of the record_version during a session
+ - check the tolerance to inconsistency of record_version during a session
+
+Ciphersuites:
+ - check the accepted ciphersuites
+ - determine the preferred ciphersuites (with different stimuli)
+ - determine the choice algorithm (directive or courteous)
+
+Compression Methods:
+ - check the accepted comrpession methods
+
+Extensions:
+ - determine the accepted extensions
+ - check for extension intolerance
+ - SNI support
+ - Secure Renego support
+ - insecure renego tolerance
+ - support for client-initiated renego
+
+Random:
+ - check for the ServerRandom quality (is it constant? does it depend on the ClientRandom?)
+
+Session resumption
+ - check wether session resumption works (session id, session ticket)
+
+SKE:
+ - quality of the DHE group
+ - quality of the DHE element (is it random? is it used only once?)
+ - quality of the ECDHE group
+ - quality of the ECDHE element (is it random? is it used only once?)
+ - support for EC
+
+CKE:
+ - check whether the CKE anti-downgrade mechanism is checked
+   * the first time
+   * in case of renegotiation or session resumption
+
+Record protocol
+ - one Handshake message spanning over several records
+ - several Handshake messages inside one record
+ - messages split in 10-byte records, in (2^14 + 1)-byte records
+ - tolerance to empty records (HS, CCS, Alert, App)
+ - tolerance to 1-byte records (HS, Alert, App)
+ - tolerance to a warning Alert between two Handhake messages
+ - tolerance to a warning Alert in the middle of a Handhake message
+ - misplacement/removal of CCS messages
+ - tolerance to warning/fatal Alerts
+ - reaction to AppData received during a Handshake (the first one or subsequent renegos)
+
+Certificates
+ - Serial numbers
+ - Duplicate certificates
+ - Unused certificates
+ - Missing certificates
+ - Algorithms/Key sizes
+ - X.509 extensions
+   * BasicConstraints
+   * Key Usage, ExtendedKeyUsage, NSCertType
+   * CertificatePolicies, EV
+   * SKI, AKI
+ - DN qulity
+ - Correct denomination of the server (CN + SAN)
+ - Validity dates
+ - Revocation state *)
+
