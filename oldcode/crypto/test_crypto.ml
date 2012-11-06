050184b35a9d9e53e5b81cbbbfd2f52a29055f8f
Olivier Levillain <olivier.levillain@picty.org>
2012-11-06 10:54:51 +0100

    Removed some old code that is not used anymore.

--- oldcode/crypto/test_crypto.ml
+++
@@ -1,5 +0,0 @@
-let _ =
-  print_endline (Common.hexdump (Crypto.md5sum ""));;
-  print_endline (Common.hexdump (Crypto.sha1sum ""));;
-
-  print_endline (Common.hexdump (Crypto.exp_mod "\x10\x20\x30" "\x01\x00\x01" "\x13\x21\x12"));;
--- oldcode/tools/categorize_answers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkfifo toto
-tools/categorize_answers.acesl /mnt/ssl-data/Campaigns/00[01]*/*.dump 2> toto | while read line; do if [ -n "$line" ]; then echo "$line"; else clear; fi; done
-
