9615dcfeaece36c6addadf229a7fc5982c9c4cd4
Olivier Levillain <olivier.levillain@picty.org>
2013-01-12 13:51:21 +0100

    Wrote some ideas for check_sslclient.

---
+++ check_sslclient.ml
@@ -0,0 +1,13 @@
+(* TODO: SSL client
+
+ - Check how subject naming is handled
+   * multiple CN
+   * wildcard
+   * empty subject
+   * IP in SAN/IPAddress
+   * IP in SAN/URL
+   * IP in SAN/DNSName
+   *  does SAN overrid CN?)
+
+ - Check wether the presence of \x00 works in CN/SAN/etc.
+*)
