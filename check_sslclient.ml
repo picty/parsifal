663d1135e0d7f12ea73a028e4013a280af357852
Olivier Levillain <olivier.levillain@picty.org>
2013-01-12 23:47:10 +0100

    Merged some older ideas for check_sslclient.

--- check_sslclient.ml
+++ check_sslclient.ml
@@ -1,13 +1,21 @@
-(* TODO: SSL client
+(* check_sslclient: TODO
 
- - Check how subject naming is handled
-   * multiple CN
-   * wildcard
-   * empty subject
-   * IP in SAN/IPAddress
-   * IP in SAN/URL
-   * IP in SAN/DNSName
-   *  does SAN overrid CN?)
+Check the version proposed?
 
- - Check wether the presence of \x00 works in CN/SAN/etc.
+[...]
+
+Check the different versions to write 0 in DHE
+Check the client avoids DHE values in {-1;0;1}
+
+Check how subject naming is handled
+ - multiple CN
+ - wildcard
+ - empty subject
+ - IP in SAN/IPAddress
+ - IP in SAN/URL
+ - IP in SAN/DNSName
+ -  does SAN overrid CN?)
+
+Check wether the presence of \x00 works in CN/SAN/etc.
+
 *)
