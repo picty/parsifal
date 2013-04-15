fbc204b2a8b5a97b1825538ce28903b7c670b615
Olivier Levillain <olivier.levillain@picty.org>
2013-04-16 00:33:03 +0200

    Updated the toplevel.sh script.

--- mess/toplevel.sh
+++ mess/toplevel.sh
@@ -6,5 +6,5 @@ rlwrap ocaml -I /usr/lib/ocaml/lwt \
 	-I /home/yeye/dev/parsifal/usrlibocaml/parsifal_net \
 	-I /home/yeye/dev/parsifal/usrlibocaml/parsifal_formats \
 	-I /home/yeye/dev/parsifal/usrlibocaml/parsifal_ssl \
+	unix.cma nums.cma bigarray.cma lwt.cma cryptokit.cma lwt-unix.cma \
 	/home/yeye/dev/parsifal/usrlibocaml/parsifal_{core,net,formats,ssl}/*.cma
-	openall.cmo
