07e7dbaf0aac5e0c7ef2fb5d8805adda59f132bd
Olivier Levillain <olivier.levillain@picty.org>
2013-04-22 23:48:37 +0200

    Micro-updated PNG to handle a little more of PNG format.

--- formats/png.ml
+++ formats/png.ml
@@ -2,18 +2,31 @@ open Lwt
 open Parsifal
 open PTypes
 
+enum color_type (8, UnknownVal UnknownColorType) =
+  | 0 -> CT_Grayscale, "grayscale"
+  | 2 -> CT_RGB, "RGB"
+  | 3 -> CT_Palette, "palette"
+  | 4 -> CT_GrayscaleAlpha, "grayscale+alpha"
+  | 6 -> CT_RGBAlpha, "RGV+alpha"
+
+
 struct image_header = {
   width : uint32;
   height : uint32;
   bit_depth : uint8;
-  color_type : uint8;
+  color_type : color_type;
   compression_method : uint8;
   filter_method : uint8;
   interlace_method : uint8;
+  (* TODO: Check consistency between bit_depth and color_type *)
 }
 
 union chunk_content [enrich] (UnparsedChunkContent) =
   | "IHDR" -> ImageHeader of image_header
+  (* PLTE is only present for some values of color_type *)
+  | "PLTE" -> Palette of list of array(3) of uint8
+  | "IDATA" -> ImageData of binstring (* TODO *)
+  | "IEND" -> ImageEnd
 
 struct chunk [with_lwt] = {
   chunk_size : uint32;
