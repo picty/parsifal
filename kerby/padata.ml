1ded5e06591e12a4c795a5b9ca1170c91b49df9b
Thomas Calderon <calderon.thomas@gmail.com>
2013-05-14 16:11:31 +0200

    Add support for ETYPE_INFO

--- kerby/padata.ml
+++ kerby/padata.ml
@@ -140,13 +140,19 @@ struct pa_pk_as_rep =
   *)
 }
 
-struct etype_info2_content =
+asn1_struct etype_info =
 {
   etype : cspe [0] of asn1 [(C_Universal, false, T_Integer)] of etype_type;
-  optional salt : cspe [1] of der_kerberos_string;
+  optional salt : cspe [1] of der_octetstring;
+}
+asn1_alias etype_infos = seq_of etype_info
+
+asn1_struct etype_info2 =
+{
+  etype : cspe [0] of asn1 [(C_Universal, false, T_Integer)] of etype_type;
+  optional salt : cspe [1] of der_octetstring;
   optional s2kparams : cspe [2] of der_octetstring
 }
-asn1_alias etype_info2
 asn1_alias etype_info2s = seq_of etype_info2
 
 (* DEBUG pa_pk_as_rep *)
