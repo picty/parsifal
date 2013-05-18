346ae15a7cd84eaf6213455962acad376796373c
Olivier Levillain <olivier.levillain@picty.org>
2013-05-19 00:45:23 +0200

    Added a relevant exit code to test_png.

--- formats/test/test_png.ml
+++ formats/test/test_png.ml
@@ -11,9 +11,10 @@ let _ =
     in
     let input = string_input_of_filename png_filename in
     let png_file = parse_png_file input in
-    print_endline (print_value (value_of_png_file png_file))
+    print_endline (print_value (value_of_png_file png_file));
+    exit 0
   with
-  | ParsingException (e, h) -> prerr_endline (string_of_exception e h)
-  | e -> prerr_endline (Printexc.to_string e)
+  | ParsingException (e, h) -> prerr_endline (string_of_exception e h); exit 1
+  | e -> prerr_endline (Printexc.to_string e); exit 1
 
  
