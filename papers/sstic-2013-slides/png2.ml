2cb5caa1e61ffaf6d6e067b6def17527ca5fe6d0
Olivier Levillain <olivier.levillain@picty.org>
2013-06-03 23:47:27 +0200

    Finalised the slides.

--- papers/sstic-2013-slides/png2.ml
+++ papers/sstic-2013-slides/png2.ml
@@ -4,8 +4,3 @@ struct png_chunk = {
   data : binstring(chunk_size);
   crc : uint32;
 }
-
-struct png_file = {
-  png_magic : magic("\x89\x50\x4e\x47\x0d\x0a\x1a\x0a");
-  chunks : list of png_chunk;
-}
