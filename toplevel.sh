bc7b1198ddd5550f7416c4ca2894dc455fc081be
Olivier Levillain <olivier.levillain@picty.org>
2013-06-15 19:23:03 +0200

    Improve toplevel.sh to compile libraries if necessary.

--- toplevel.sh
+++ toplevel.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
+set -e
+
+
+# Script initialization
+
+PROGNAME=$0
+
+error () {
+	echo "Error: $1" >&2
+	echo "Usage: $0 [parsifal dir]" >&2
+	exit 1
+}
+
+info () {
+    [ -z "$VERBOSE" ] || echo "[INFO] $1" >&2
+}
+
+PARSIFAL_DIR=$1
+[ -z "$PARSIFAL_DIR" ] && PARSIFAL_DIR="$(dirname "$PROGNAME")"
+
+[ -f "$PARSIFAL_DIR/Makefile.ocaml" ] || error "$PARSIFAL_DIR does not seem to be a directory containing Parsifal."
+
+
+
+info "Compiling everything if necessary"
+cd "$PARSIFAL_DIR"
+make libs
+cd -
+
 rlwrap ocaml -I /usr/lib/ocaml/lwt \
 	-I /usr/lib/ocaml/cryptokit \
-	-I /home/yeye/dev/parsifal/usrlibocaml/parsifal_core \
-	-I /home/yeye/dev/parsifal/usrlibocaml/parsifal_net \
-	-I /home/yeye/dev/parsifal/usrlibocaml/parsifal_formats \
-	-I /home/yeye/dev/parsifal/usrlibocaml/parsifal_ssl \
+	-I "$PARSIFAL_DIR/usrlibocaml/parsifal_core" \
+	-I "$PARSIFAL_DIR/usrlibocaml/parsifal_net" \
+	-I "$PARSIFAL_DIR/usrlibocaml/parsifal_formats" \
+	-I "$PARSIFAL_DIR/usrlibocaml/parsifal_ssl" \
 	unix.cma nums.cma bigarray.cma lwt.cma cryptokit.cma lwt-unix.cma \
-	/home/yeye/dev/parsifal/usrlibocaml/parsifal_{core,net,formats,ssl}/*.cma
+	"$PARSIFAL_DIR"/usrlibocaml/parsifal_{core,net,formats,ssl}/*.cma
