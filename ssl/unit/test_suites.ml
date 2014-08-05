ee5455ffb8adc87e46e8e39242655aad5c0f05db
Olivier Levillain <olivier.levillain@picty.org>
2014-08-05 21:25:43 +0200

    New unit tests on record encryption/decryption: check when integrity should fail.
    It is essential in security software to *also* check that what should not work does indeed not work.
    That said, do not use this implementation for anything real: its goal is to build analysis tools.

--- ssl/unit/test_suites.ml
+++ ssl/unit/test_suites.ml
@@ -33,8 +33,31 @@ let mk_idempotence_suite name encrypt decrypt n maxlen =
   mk_one_test [] n maxlen
 
 
+let test_integrity encrypt decrypt data =
+  let encrypted_msg = encrypt data in
+  (* TODO: Choose the position and the new value using Random *)
+  let len = String.length encrypted_msg in
+  if encrypted_msg.[len / 2] = '\x00'
+  then encrypted_msg.[len / 2] <- 'A'
+  else encrypted_msg.[len / 2] <- '\x00';
+  (* TODO *)
+  let integrity, _ =  decrypt encrypted_msg in
+  assert_bool "integrity check should have failed" (not integrity)
+
+let mk_check_integrity_check name encrypt decrypt _n maxlen =
+  let rec mk_one_test accu i curlen = match i, curlen with
+    | 0, 0 -> accu
+    | 0, _ -> mk_one_test accu 1 (* TODO: n *) (curlen-1)
+    | _, _ ->
+      let t_name = name ^ "_integrity_check_" ^ (string_of_int curlen) ^ "_" ^ (string_of_int i)
+      and t_fun = fun () -> test_integrity encrypt decrypt (random_string curlen) in
+      mk_one_test ((t_name >:: t_fun)::accu) (i-1) curlen
+  in
+  (* TODO: When test_integrity is randomised, make n a variable *)
+  mk_one_test [] 1 (* TODO: n *) maxlen
+
+
 (* TODO:
-   - add negative checks (ciphertext alteration should lead to fails)
    - add full key derivation, from PMS+CR+SR to encryption and decryption *)
 
 
@@ -48,6 +71,11 @@ let tests = List.flatten [
   mk_idempotence_suite "RC4_MD5" (rc4_encrypt hmac_md5 16 (String.make 16 'A') (String.make 16 'B'))
     (rc4_decrypt hmac_md5 16 (String.make 16 'A') (String.make 16 'B')) 10 20;
   mk_idempotence_suite "RC4_SHA1" (rc4_encrypt hmac_sha1 20 (String.make 20 'A') (String.make 16 'B'))
+    (rc4_decrypt hmac_sha1 20 (String.make 20 'A') (String.make 16 'B')) 10 20;
+
+  mk_check_integrity_check "RC4_MD5" (rc4_encrypt hmac_md5 16 (String.make 16 'A') (String.make 16 'B'))
+    (rc4_decrypt hmac_md5 16 (String.make 16 'A') (String.make 16 'B')) 10 20;
+  mk_check_integrity_check "RC4_SHA1" (rc4_encrypt hmac_sha1 20 (String.make 20 'A') (String.make 16 'B'))
     (rc4_decrypt hmac_sha1 20 (String.make 20 'A') (String.make 16 'B')) 10 20;
 ]
 
