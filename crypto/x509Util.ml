ac13602056852454ee3a63b7cdfbb29979908f40
Olivier Levillain <olivier.levillain@ssi.gouv.fr>
2014-08-14 15:20:53 +0200

    Add a wrapper around X.509 certificate: smart_certificate.
    Yet I probably should not be too liberal about the term smart.

---
+++ crypto/x509Util.ml
@@ -0,0 +1,118 @@
+open X509
+
+
+(*************************************)
+(* smart_certificate                 *)
+(* Wrapper around X.509 certificates *)
+(*************************************)
+
+type smart_certificate = {
+  name : string;
+  trusted_cert : bool;
+  mutable raw_value : string option;
+  mutable cert_hash : string option;
+  mutable parsed_cert : certificate option;
+  mutable pos_in_hs_msg : int option;
+  mutable subject_hash : string option;
+  mutable issuer_hash : string option;
+  issued_certs : (string, bool) Hashtbl.t;
+}
+
+
+let sc_of_raw_value name trusted_cert r = {
+  name; trusted_cert;
+  raw_value = Some r; cert_hash = None;
+  parsed_cert = None; pos_in_hs_msg = None;
+  subject_hash = None; issuer_hash = None;
+  issued_certs = Hashtbl.create 10;
+}
+
+let sc_of_raw_value_and_cert name trusted_cert r c = {
+  name; trusted_cert;
+  raw_value = Some r; cert_hash = None;
+  parsed_cert = Some c; pos_in_hs_msg = None;
+  subject_hash = None; issuer_hash = None;
+  issued_certs = Hashtbl.create 10;
+}
+
+let sc_of_cert name trusted_cert c = {
+  name; trusted_cert;
+  raw_value = None; cert_hash = None;
+  parsed_cert = Some c; pos_in_hs_msg = None;
+  subject_hash = None; issuer_hash = None;
+  issued_certs = Hashtbl.create 10;
+}
+
+
+let raw_value_of_sc sc = match sc.raw_value, sc.parsed_cert with
+  | Some r, _ -> r
+  | None, Some c ->
+    let r = Parsifal.exact_dump dump_certificate c in
+    sc.raw_value <- Some r;
+    r
+  | None, None -> failwith "raw_value_of_sc"
+
+let hash_of_sc sc = match sc.cert_hash with
+  | Some h -> h
+  | None ->
+    let r = raw_value_of_sc sc in
+    let h = CryptoUtil.sha1sum r in
+    sc.cert_hash <- Some h;
+    h
+
+let cert_of_sc sc = match sc.parsed_cert with
+  | Some c -> c
+  | None ->
+    let r = raw_value_of_sc sc in
+    let c = parse_certificate (Parsifal.input_of_string sc.name r) in
+    sc.parsed_cert <- Some c;
+    c
+
+let issuer_hash_of_sc sc = match sc.issuer_hash with
+  | Some h -> h
+  | None ->
+    let parsed_c = cert_of_sc sc in
+    let h = match parsed_c.tbsCertificate.issuer_raw with
+      | Some s -> CryptoUtil.sha1sum s
+      | None -> failwith "issuer_hash_of_sc" (* TODO: dump the dn and update cert? *)
+    in
+    sc.issuer_hash <- Some h;
+    h
+
+let subject_hash_of_sc sc = match sc.subject_hash with
+  | Some h -> h
+  | None ->
+    let parsed_c = cert_of_sc sc in
+    let h = match parsed_c.tbsCertificate.subject_raw with
+      | Some s -> CryptoUtil.sha1sum s
+      | None -> failwith "subject_hash_of_sc" (* TODO: dump the dn and update cert? *)
+    in
+    sc.subject_hash <- Some h;
+    h
+
+let cert_id_of_sc sc =
+  let c = cert_of_sc sc in
+  subject_hash_of_sc sc, c.tbsCertificate.subjectPublicKeyInfo
+
+let parse_smart_cert trusted_cert input =
+  let saved_offset = PTypes.parse_save_offset input in
+  let cert = parse_certificate input in
+  match PTypes.parse_raw_value saved_offset input with
+  | None -> sc_of_cert input.Parsifal.cur_name trusted_cert cert
+  | Some raw_value -> sc_of_raw_value_and_cert input.Parsifal.cur_name trusted_cert raw_value cert
+
+let sc_of_cert_in_hs_msg trusted_cert name i = function
+  | PTypes.Parsed (raw_opt, parsed_c) ->
+    { name; trusted_cert;
+      raw_value = raw_opt; cert_hash = None;
+      parsed_cert = Some parsed_c; pos_in_hs_msg = Some i;
+      subject_hash = None; issuer_hash = None;
+      issued_certs = Hashtbl.create 10;
+    }
+  | PTypes.Unparsed raw ->
+    { name; trusted_cert;
+      raw_value = Some raw; cert_hash = None;
+      parsed_cert = None; pos_in_hs_msg = Some i;
+      subject_hash = None; issuer_hash = None;
+      issued_certs = Hashtbl.create 10;
+    }
